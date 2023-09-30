c8e142011d33e02799f995b564a07ebd36e24f97
Tayyaba Shahid <136459395+TAYYABASHAHID1@users.noreply.github.com>
2023-09-30 12:55:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+var messages="hello world"
+alert(messages)
+
+
+
+
+
+var username;
+
+
+var myName = "John Doe";
+alert(myName)
+
+console.log(myName)
+
+
+
+
+
+var studentName = "john Doe";
+var studentAge = 20;
+var studentcourse = "certified web application development"
+
+
+
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Student course: " + studentcourse);
+
+
+
+
+alert("pizza\n pizz\n piz\n pi\n p");
+
+
+var email="example@example.com"
+
+console.log("my email address is", email );
+
+
+var symbols="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(symbols)
